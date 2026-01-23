469d234b4cbfde279e26f0abbf537c4429dd7597
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-23 12:23:56 +0530

    Update Home page with custom components

--- App.tsx
+++ App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import { Text, View, Button, Alert } from 'react-native';
+import MyComponent from './src/components/MyComponent';
 
 function App() {
 
   return (
       <View style={{ alignItems: 'center'}}>
-      <Text style={{color: 'red',
-          fontSize: 30,
-          fontWeight: 'bold',
-          textAlign: 'center',
-          marginTop: 200
-          }}>
-      Hello world</Text>
-      <Text style={{fontSize: 25,
-          color: 'blue',
-          marginBottom: 15
-          }}>Welcome to
-      React Native</Text>
-    <Button title="Click Me" onPress={() => Alert.alert('Button Clicked')}/>
+      <MyComponent />
       </View>
   );
 
