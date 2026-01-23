ee2a5bdd8ecad92a6debf56abc81d5df8b18e4aa
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-23 12:23:29 +0530

    Create Custom Component

---
+++ src/components/CustomLable.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustomLable = () => {
+  return (
+    <View>
+      <Text style={{
+        fontSize: 40,
+        color: 'blue',
+        fontWeight: '700'
+      }}>CustomLable</Text>
+    </View>
+  )
+}
+
+export default CustomLable
