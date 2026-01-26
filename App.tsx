4f58d2401d1349a92c40f17ecc6de4d47649f817
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-26 13:44:10 +0530

    Develop text component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
 import CustomLable from './src/components/CustomLable';
 
 function App() {
@@ -17,6 +17,16 @@ function App() {
       <View style={{ alignItems: 'center'}}>
         <CustomLable>Welcome !</CustomLable>
         <CustomText fsize={25} a={'Hello'} b={'World'}/>
+        <TextInput style={{ height: 40, borderColor: 'black', color: 'blue', borderWidth: 2, width: 200, marginTop: 20, marginBottom: 20, borderRadius: 10, backgroundColor: 'gray', paddingLeft: 10 }}
+        
+        placeholder='User Name'
+        placeholderTextColor={'white'}/>
+
+         <TextInput style={{ height: 40, borderColor: 'black', color: 'blue', borderWidth: 2, width: 200, marginTop: 20, marginBottom: 20, borderRadius: 10, backgroundColor: 'gray', paddingLeft: 10 }}
+        
+        placeholder='Password'
+        placeholderTextColor={'white'}/>
+
         <Button title='Click me' onPress={() => {
           click(10,5);
         }} />
